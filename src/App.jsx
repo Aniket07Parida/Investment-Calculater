6197fea14bcc9d42b3e833463268860321c79383
Aniket <aniketparida666@gmail.com>
2025-03-20 16:55:23 +0530

    Investment Calculater

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,28 @@
-import React, { useState } from "react";
-import CoreComponents from "./CoreComponents";
-import { Data } from "./Data";
-import { EXAMPLES } from "./Example";
-import About from "./About";
-import ButtonHandle from "./Button";
-
-import './index.css'; 
+import React from "react";
+import { useState } from "react";
+import Result from "./Result";
+import Inputs from "./Input";
+import Heading from "./Heading";
 
 export default function App() {
-    const [content, setContent] = useState(null);
-
-    function handelSelect(selectedButton) {
-        setContent(selectedButton);
+    const [InputValue, setInputValue] = useState({ initialInvestment: 10000, annualInvestment: 1200, expectedReturn: 6, duration: 10 });
+    const inputValid = InputValue.duration >= 1;
+    
+    function handelChange(Inputidentyfier, newValue) {
+        setInputValue(prevInput => {
+            return {
+                ...prevInput,
+                [Inputidentyfier]:+newValue
+            }
+        })
     }
 
     return (
         <>
-            <header>
-                <h1>React Essential</h1>
-                <p>Core React concepts you will need for almost any app you are going to build!</p>
-            </header>
-            <div className="container">
-                <h3>Core Concepts</h3>
-                <ul>
-                    {Data.map(item => 
-                        <CoreComponents 
-                            key={item.id}
-                            title={item.title}
-                            description={item.description}
-                            img={item.image} />
-                    )}
-                </ul>
-            </div>
-            
-            <h2 id="example">Example</h2>
-            <menu className="Button">
-                <ButtonHandle onSelct={() => handelSelect('components')} text={'Components'}/>
-                <ButtonHandle onSelct={() => handelSelect('jsx')} text={'JSX'}/>
-                <ButtonHandle onSelct={() => handelSelect('props')} text={'Props'}/>
-                <ButtonHandle onSelct={() => handelSelect('state')} text={'State'}/>
-            </menu>
-            <div className="contant" >
-                {!content && <p>Please select a topic.</p>}
-                {content && EXAMPLES[content] && (
-                    <About 
-                        title={EXAMPLES[content].title}
-                        description={EXAMPLES[content].description}
-                        code={EXAMPLES[content].code}
-                    />
-                )}
-            </div>
+            <Heading />
+            <Inputs InputValue={InputValue} onChange={handelChange} />
+            {!inputValid && <p className="center">Plese Enter duration grater then Zero</p>}
+            {inputValid && <Result Output={InputValue} /> }
         </>
-    );
+    )
 }
---
+++ src/Heading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Heading() {
+    return (
+        <>
+            <img src="https://investment-calculator-app-react.netlify.app/assets/investment-calculator-logo-fb67e9a9.png" alt="my-Image" />
+            <h1 className="heading">Investment Calculater</h1>
+        </>
+    )
+}
